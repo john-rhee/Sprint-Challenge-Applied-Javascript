50bff1017c7c4a66ea19e39651648b2203c69dd4
Johnnyr81 <emailjohn81@gmail.com>
2019-10-04 18:08:21 -0500

    update

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,28 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header(date,title,temp) {
+
+    const varHeader = document.createElement('div');
+    const varDate = document.createElement('span');
+    const varTitle = document.createElement('h1');
+    const varTemp = document.createElement('span');
+  
+    varDate.textContent = date;
+    varTitle.textContent = title;
+    varTemp.textContent = temp;
+  
+    varHeader.classList.add("header");
+    varDate.classList.add("date");
+    varTemp.classList.add("temp");
+  
+    varHeader.appendChild(varDate);
+    varHeader.appendChild(varTitle);
+    varHeader.appendChild(varTemp);
+  
+    return varHeader;
+  };
+  
+  const headerContainer = document.querySelector('.header-container');
+  
+  headerContainer.appendChild(Header('MARCH 28, 2019','Lambda Times','98°'));
